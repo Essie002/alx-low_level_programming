042907340084bdf6eb32a7baf415b6c15c50cb4c
Essie002 <essiemugure02@gmail.com>
2022-06-27 08:19:36 -0700

    Created a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -prints "Programming is like building a multilingual puzzle
+ * return 0 - if exited properly otherwise non zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
